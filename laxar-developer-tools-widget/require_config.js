eeedaff216fdf1d768c9d452e7b7c90b1a22abbd
Jonas Schulte <jonas.schulte@aixigo.de>
2016-10-05 18:56:07 +0200

    release v0.2.0

---
+++ laxar-developer-tools-widget/require_config.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright 2016 aixigo AG
+ * Released under the MIT license.
+ * http://laxarjs.org/license
+ */
+/* global -require */
+var require = {
+   paths: {
+      'laxar-patterns': 'laxar-patterns/dist/laxar-patterns',
+      'json-patch': 'fast-json-patch/src/json-patch-duplex'
+   }
+};
